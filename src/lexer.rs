70b406b4faff4aa3ca6a775213ed0b8ec49bdf57
Rory N <rorynickolls@gmail.com>
2019-02-25 22:54:02 +0000

    Added lexer for simple arithmetic expressions

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,22 +1,51 @@
-use crate::regex::RegexSet;
+use crate::regex::Regex;
 
-enum Token {
-    ADD,
-    SUB,
-    MUL,
-    DIV,
+#[derive(Copy, Clone, Debug)]
+pub enum Token {
+    TokenAdd,
+    TokenSub,
+    TokenMul,
+    TokenDiv,
+    TokenNumber,
 }
 
-struct Lexer {
-
+pub struct Lexer {
+    token_expressions: Vec<(Regex, Token)>,
 }
 
 impl Lexer {
     pub fn new() -> Lexer {
-        Lexer { } 
+        let mut token_expressions = Vec::new();
+        token_expressions.push((Regex::new(r"^\+$").unwrap(), Token::TokenAdd));
+        token_expressions.push((Regex::new(r"^\-$").unwrap(), Token::TokenSub));
+        token_expressions.push((Regex::new(r"^\*$").unwrap(), Token::TokenMul));
+        token_expressions.push((Regex::new(r"^/$").unwrap(), Token::TokenDiv));
+        token_expressions.push((Regex::new(r"^\d+$").unwrap(), Token::TokenNumber));
+        Lexer { token_expressions }
     }
 
-    pub fn tokenize(file: &str) {
+    pub fn tokenize(&self, contents: String) -> Vec<Token> {
+        let mut tokens = Vec::new();
 
+        let mut remaining_contents = contents;
+        while let Some(token) = self.next_token(&mut remaining_contents) {
+            tokens.push(token);
+            println!("{:?}, {}", token, remaining_contents);
+        }
+        tokens
+    }
+
+    fn next_token(&self, contents: &mut String) -> Option<Token> {
+        let mut range = contents.len();
+        while range > 0 {
+            for (regex, tok) in self.token_expressions.iter() {
+                if regex.is_match(&contents[0..range]) {
+                    *contents = contents[range..].to_string();
+                    return Some(*tok);
+                }
+            }
+            range = range - 1;
+        }
+        return None;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 pub mod lexer;
+use lexer::Lexer;
+
 extern crate regex;
 
 fn main() {
-    println!("Hello, world!");
+    let lexer = Lexer::new();
+    let tokens = lexer.tokenize(String::from("330+300"));
+    println!("{:?}", tokens);
 }
